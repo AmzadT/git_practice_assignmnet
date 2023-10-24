1750ef8ce92052bfae838c09571c135eea35872c
AmzadT <amzadhussain7250@gmail.com>
2023-10-24 17:23:12 +0530

    check prime numbers

---
+++ prime.js
@@ -0,0 +1,19 @@
+var number = 17;
+var count = 0;
+
+for(var i =0; i>number; i++)
+{
+    if(number%2==1)
+    {
+        count++;
+    }
+}
+
+if(count==2)
+{
+    console.log("Prime Number")
+}
+else
+{
+console.log("Prime Number")
+}
