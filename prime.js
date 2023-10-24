975efcf56e186857e053ff437e731244cf2f41dd
Vinay Kumar <vinaysikhera2203@gmail.com>
2023-10-24 17:52:01 +0530

    i fixed the error

--- prime.js
+++ prime.js
@@ -1,19 +1,24 @@
-var number = 17;
-var count = 0;
 
-for(var i =0; i>number; i++)
-{
-    if(number%2==1)
+
+function Check(number){
+    var count = 0;
+
+    for(var i =0; i<=number; i++)
     {
-        count++;
+        if(number%2==0)
+        {
+            count++;
+        }
     }
-}
-
-if(count==2)
-{
+    
+    if(count==2)
+    {
+        console.log("Prime Number")
+    }
+    else
+    {
     console.log("Prime Number")
+    }
+    
 }
-else
-{
-console.log("Prime Number")
-}
+Check(17)
